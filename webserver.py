ac5150ab1d32fbff73f5b57f1eac4ce820b2c465
Jeremy Suurkivi <suurkivi@gmail.com>
2018-03-14 17:21:30 +0900

    insecure_compare()

--- webserver.py
+++ webserver.py
@@ -1,10 +1,31 @@
 import http.server
+import time
 import urllib.parse
 
 import util
 from s4c31 import hmac_sha1
 
 KEY = util.random_word()
+
+
+def insecure_compare(artificial_delay: float, s0: str, s1: str) -> bool:
+    # TODO: needs tests
+    if len(s1) > len(s0):
+        s0, s1 = s1, s0  # make s0 the longer of the two
+    l0, l1 = list(s0), list(s1)
+    l1 += [None] * (len(l0) - len(l1))  # pad l1 if needed
+
+    assert len(l0) == len(l1)
+
+    for c0, c1 in zip(l0, l1):
+        ok = c0 == c1
+        time.sleep(artificial_delay)
+        if not ok:
+            print("")
+            return False
+        print(c0, end='', flush=True)
+
+    return True
 
 
 class Server(http.server.BaseHTTPRequestHandler):
@@ -35,11 +56,16 @@ class Server(http.server.BaseHTTPRequestHandler):
         return b"yes" if mac == target else b"no"
 
 
-def main():
+def serve():
     addr = ('127.0.0.1', 8000)
     print("Listening on", addr)
     s = http.server.HTTPServer(addr, Server)
     s.serve_forever()
+
+
+def main():
+    # serve()
+    print(insecure_compare(0.50, "abcd", "abcd"))
 
 
 if __name__ == '__main__':
